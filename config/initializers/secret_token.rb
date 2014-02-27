745870e0798da6f851eff6f49a33a4d0a66132c9
jordansaxe <jordansaxe15@gmail.com>
2014-02-27 15:16:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Browsify::Application.config.secret_token = '785c13df8a5937f4727f714265216e21206d7e3e1bb3f0e843fbd41ea3c46a2f08196cb9e25b58895bffe934c6e2a6cce5dd7fcae1ad255131a758dccb41ee20'
